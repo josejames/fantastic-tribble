e01fc12dc856efb18af2897fce4b3494e0c1d82f
Jaime <cepiscorp@gmail.com>
2016-02-21 12:25:11 -0600

    fixing pdf view

--- controller/generarReporte.php
+++ controller/generarReporte.php
@@ -1,33 +1,30 @@
 <?php
-    /* Object Oriented */
-    /////////////////////////
+    
+    $id_tour = $_GET['id'];
+    $horario = $_GET['hora'];
+    $date = $_GET['date'];
+    $table_header = "";
 
     include("../mpdf/mpdf.php");
     $mpdf = new mPDF(”);
-    //$mpdf = new mPDF('c','A4','','',32,25,27,25,16,13); 
-
+    
+    //the table stylesheet
     $stylesheet = file_get_contents('../mpdf/examples/mpdfstyletables.css');
     $mpdf->WriteHTML($stylesheet,1);
     //$mpdf->useDefaultCSS2 = true;
     
-
+    //set header and footer to the pdf
     $mpdf->SetHeader('{DATE j-m-Y  h:i:s}| Reporte de Operadora |{PAGENO}');
     $mpdf->SetFooter('|Printed using mPDF|');
 
 
-    //////////////////////////
     $texto = "ERROR: ";
 
-        $id_tour = $_GET['id'];
-        $horario = $_GET['hora'];
-        $date = $_GET['date'];
-
         //archivo de configuracion
-        include '../controller/config.php';
+        include 'config.php';
 
         $mysqli = new mysqli($hostdb, $usuariodb, $clavedb, $nombredb);
-        //$conn = mysql_connect($hostdb, $usuariodb, $clavedb);
-
+       
         /* comprobar la conexión */
         if (mysqli_connect_errno()) {
             echo $texto . mysqli_connect_error();
@@ -42,7 +39,10 @@
                 $mpdf->WriteHTML('<p> </p>');
                 $mpdf->WriteHTML('<p> </p>');
                 $mpdf->WriteHTML('<div>');
-                $mpdf->WriteHTML($tour_name[0]);
+
+                $table_header = $tour_name[0];
+                //$mpdf->WriteHTML($table_header);
+                
                 $mpdf->WriteHTML('</div>');
 
                 $mpdf->WriteHTML('<p> </p>');
@@ -57,36 +57,46 @@
 
         if ($resultado = $mysqli->query($consulta)) {
 
-            $mpdf->WriteHTML("<table width='100%' class='bpmTopic'>");
-            $mpdf->WriteHTML("<thead></thead>");
+            $mpdf->WriteHTML("<table width='100%' class='bpmTopnTail'>");
+            $mpdf->WriteHTML("<thead><tr><th colspan='3'>");
+            $mpdf->WriteHTML($table_header);
+            $mpdf->WriteHTML("</th></tr></thead>");
+
             $mpdf->WriteHTML("<tbody>");
-            $mpdf->WriteHTML("<tr> <th> Cliente </th>");
-            $mpdf->WriteHTML("<th> Hotel </th>");
-            $mpdf->WriteHTML("<th> Habitacion </th>");
-            $mpdf->WriteHTML("<th> Adultos </th>");
-            $mpdf->WriteHTML("<th> Niños </th>");
-            $mpdf->WriteHTML("<th> Insen </th>");
+            $mpdf->WriteHTML("<tr> <th> No. Hab. </th>");
+            $mpdf->WriteHTML("<th> CLIENTE </th>");
+            $mpdf->WriteHTML("<th> ADULTOS </th>");
+            $mpdf->WriteHTML("<th> MENORES </th>");
+            $mpdf->WriteHTML("<th> INSEN </th>");
+            $mpdf->WriteHTML("<th> HORARIO </th>");
+            $mpdf->WriteHTML("<th> HOTEL </th>");
             $mpdf->WriteHTML("</tr>");
 
 
             /* obtener el array de objetos */
             while ($fila = $resultado->fetch_row()) {
                 //printf ("%s (%s)\n", $fila[0], $fila[1]);                                
-                $data .= "<tr id=".$fila[0]." >\n";//id
+                $data .= "<tr id=".$fila[0]." >\n";//id de la reserva
+
+                    //columnas
+                    $data .= "<td>".$fila[3]."</td>\n";//Num Habitacion
                     $data .= "<td>".$fila[1]."</td>\n";//Nombre cliente
+                    $data .= "<td>".$fila[4]."</td>\n";//adultos
+                    $data .= "<td>".$fila[5]."</td>\n";//ninos
+                    $data .= "<td>".$fila[6]."</td>\n";//insen
+                    $data .= "<td>".$horario."</td>\n";//horario
+
 
                     $consulta_name = "SELECT i.nombre_hotel FROM institucion i WHERE i.clave_hotel = '".$fila[2]."'";
                     
                     if ($resultado_name = $mysqli->query($consulta_name)) {
                         $fila_name = $resultado_name->fetch_row();
-                        $data .= "<td>".$fila_name[0]."</td>\n";//Clave Hotel
+                        $data .= "<td>".$fila_name[0]."</td>\n";//Nombre Hotel
                     }else{
                         $data .= "<td>".$fila[2]."</td>\n";//Clave Hotel
                     }
-                    $data .= "<td>".$fila[3]."</td>\n";//Num Habitacion
-                    $data .= "<td>".$fila[4]."</td>\n";//adultos
-                    $data .= "<td>".$fila[5]."</td>\n";//ninos
-                    $data .= "<td>".$fila[6]."</td>\n";//insen
+                    
+                    
                 $data .= "</tr>\n";
                 $mpdf->WriteHTML($data);
                 $data = "";
